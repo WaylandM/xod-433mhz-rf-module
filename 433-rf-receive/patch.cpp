942dbd0867fc1b42345f5bda0d5612aba127efd5
Matt <mw283@cam.ac.uk>
2019-10-20 14:58:40 +0100

    modified:   433-rf-receive/patch.cpp

--- 433-rf-receive/patch.cpp
+++ 433-rf-receive/patch.cpp
@@ -9,31 +9,19 @@ struct State {
 
 void evaluate(Context ctx) {
     auto state = getState(ctx);
-    
-    
+
     // The node responds only if there is an input pulse
     if (!isInputDirty<input_UPD>(ctx))
         return;
 
-    
-
     // Get a pointer to the `RH_ASK` class instance
     auto driver = getValue<input_DEV>(ctx);
 
-    
     uint8_t buflen = sizeof(state->buf);
-
-    
-    //uint8_t arr[] = "sausage";
-    //view = CStringView(arr);
 
     if (driver->recv(state->buf, &buflen)) // Non-blocking
     {
-        //state->view = CStringView((char*)buf);
-        //uint8_t arr[] = "sausage\0";
-        //state->view = CStringView((char*)arr);
         emitValue<output_MSG>(ctx, XString(&state->view));
         emitValue<output_REC>(ctx, 1);
     }
-    
 }
