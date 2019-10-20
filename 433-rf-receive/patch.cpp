3a9c2504b723c9d858fa455f0997291f061dad16
Matt <mw283@cam.ac.uk>
2019-10-20 14:51:35 +0100

    modified:   433-rf-receive/patch.cpp 	modified:   example1-com-test-receiver/patch.xodp

--- 433-rf-receive/patch.cpp
+++ 433-rf-receive/patch.cpp
@@ -1,40 +1,39 @@
 
 struct State {
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    CStringView view;
+    State() : view(buf) {}
 };
 
 {{ GENERATED_CODE }}
 
 void evaluate(Context ctx) {
+    auto state = getState(ctx);
+    
+    
     // The node responds only if there is an input pulse
     if (!isInputDirty<input_UPD>(ctx))
         return;
 
+    
+
     // Get a pointer to the `RH_ASK` class instance
     auto driver = getValue<input_DEV>(ctx);
 
-    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
-    uint8_t buflen = sizeof(buf);
-
-    if (driver->recv(buf, &buflen)) // Non-blocking
-    {
-  //int i;
-
-  // Message with a good checksum received, dump it.
-  //driver.printBuffer("Got:", (char*)buf, buflen);
-
-    //XString msg[buflen];
-    //for(int i=0;i<buflen;i++)
-        //msg[i]=buf[i];
-
-        //uint8_t msg[buflen+1];
-        //uint8_t i;
-        //for(i=0; i<buflen; i++)
-          //  msg += buf[i];
     
-        //msg += '\0';
+    uint8_t buflen = sizeof(state->buf);
 
-        emitValue<output_MSG>(ctx, XString(buf));
+    
+    //uint8_t arr[] = "sausage";
+    //view = CStringView(arr);
+
+    if (driver->recv(state->buf, &buflen)) // Non-blocking
+    {
+        //state->view = CStringView((char*)buf);
+        //uint8_t arr[] = "sausage\0";
+        //state->view = CStringView((char*)arr);
+        emitValue<output_MSG>(ctx, XString(&state->view));
         emitValue<output_REC>(ctx, 1);
-        
     }
+    
 }
